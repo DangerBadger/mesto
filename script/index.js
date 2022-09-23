05e0f170eb85a0018bba2175b88acef3dbe8b54c
Kirill Chivchiev <chiefchief.92@mail.ru>
2022-09-24 00:24:05 +0700

    Feature: Add popup

---
+++ script/index.js
@@ -0,0 +1,32 @@
+const popupOpenButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.close-btn');
+let formElement = popup.querySelector('.popup__form');
+const saveButton = formElement.querySelector('.popup__save-btn');
+let nameInput = formElement.querySelector('.popup__form-input_el_name');
+let jobInput = formElement.querySelector('.popup__form-input_el_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+function popupToggle() {
+  popup.classList.toggle('popup_opened');
+}
+
+popupCloseButton.addEventListener('click', popupToggle);
+saveButton.addEventListener('click', popupToggle);
+
+function fillInput() {
+  popupToggle();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+popupOpenButton.addEventListener('click', fillInput);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
